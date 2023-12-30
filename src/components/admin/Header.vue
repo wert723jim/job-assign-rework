8dbc21031baa331a58e2904d56ae71e469a689d8
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-12-30 16:41:30 +0800

    feat: Header logout button and show nickname on Header

--- src/components/admin/Header.vue
+++ src/components/admin/Header.vue
@@ -8,12 +8,12 @@
             <path fill-rule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zM4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM10 8c.552 0 1-.672 1-1.5S10.552 5 10 5s-1 .672-1 1.5S9.448 8 10 8z"/>
         </svg>
         <!--可以使用图片,大小20,class='rounded-circle'-->
-        BsAdmin
+        {{ nickname }}
       </button>
 
 
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
-        <a class="dropdown-item" href="out.html">登出</a>
+        <a class="dropdown-item" @click.stop.prevent="logout">登出</a>
       </div>
     </div>
   </header>
@@ -29,6 +29,11 @@ const router = useRouter()
 const token = localStorage.getItem('token')
 if (!token) {
   router.replace('/login')
+}
+
+const logout = () => {
+  localStorage.setItem('token', '')
+  router.push('/')
 }
 
 onMounted(async () => {
