5fe6d17b999a83eb9cc88552f48b1985f58f876e
Allen_Yeh <happy1294123@gmail.com>
2023-12-30 16:42:05 +0800

    fix: file upload error

--- src/components/admin/form/CreateProductForm.vue
+++ src/components/admin/form/CreateProductForm.vue
@@ -3,43 +3,70 @@
     <form @submit.stop.prevent="confirmForm">
       <div class="form-group">
         <label for="productName">商品名稱: </label>
-        <input type="text" class="form-control" id="productName" v-model="formDetail.name">
+        <input
+          type="text"
+          class="form-control"
+          id="productName"
+          v-model="formDetail.name"
+        >
       </div>
       <div class="form-group">
         <label for="productIsDisplay">發布狀態: </label>
-        <select id="productIsDisplay" v-model="formDetail.isDisplay">
+        <select
+          id="productIsDisplay"
+          v-model="formDetail.isDisplay"
+        >
           <option :value="true">顯示</option>
           <option :value="false">隱藏</option>
         </select>
       </div>
       <div class="form-group">
         <label for="productIsOpen">訂單狀態: </label>
-        <select id="productIsOpen" v-model="formDetail.isOpen">
+        <select
+          id="productIsOpen"
+          v-model="formDetail.isOpen"
+        >
           <option :value="true">啟用</option>
           <option :value="false">停用</option>
         </select>
       </div>
       <div class="form-group">
         <label for="productUrl">訂單連結: </label>
-        <input type="text" class="form-control" id="productUrl" v-model="formDetail.url">
+        <input
+          type="text"
+          class="form-control"
+          id="productUrl"
+          v-model="formDetail.url"
+        >
       </div>
       <div class="form-group">
         <label for="productImgUpload">上傳圖片: </label>
         <small>(建議大小500px*500px，不超過1.5MB)</small>
         <div class="w-50">
-          <img :src="productImgPreview" alt="">
+          <img
+            :src="productImgPreview"
+            alt=""
+          >
         </div>
-        <input type="file" id="productImgUpload" ref="uploadInput" @change="previewFile">
+        <input
+          type="file"
+          id="productImgUpload"
+          ref="uploadInput"
+          @change="previewFile"
+        >
         <button @click.prevent.stop="upload">上傳</button>
       </div>
-      <button type="submit" class="btn btn-primary">確認</button>
+      <button
+        type="submit"
+        class="btn btn-primary"
+      >確認</button>
     </form>
   </div>
 </template>
 
 <script setup>
 import { reactive, onMounted } from 'vue'
-import fetchWithToken from '@utils/fetchFn.js'
+import { fetchUploadFileWithToken } from '@utils/fetchFn.js'
 const props = defineProps({
   productInfo: {
     type: Object,
@@ -55,7 +82,7 @@ const props = defineProps({
 const emit = defineEmits(['confirmForm'])
 
 onMounted(() => {
-  Object.assign(formDetail, {...props.productInfo})
+  Object.assign(formDetail, { ...props.productInfo })
 })
 
 const formDetail = reactive({
@@ -70,32 +97,34 @@ const productImgPreview = ref('')
 const previewFile = (event) => {
   const files = event.target.files
   formDetail.files = files
-  if(files.length === 0) {
+  if (files.length === 0) {
     productImgPreview.value = ''
-  }else {
+  } else {
     const imageURL = window.URL.createObjectURL(files[0])
     productImgPreview.value = imageURL
   }
 }
 
 const upload = async () => {
-  const fileId = await fetchWithToken('/api/upload', 'POST', {
-    files: formDetail.files
-  })
-  console.log(formDetail.files)
+  const formData = new FormData()
+  formData.append('files', formDetail.files[0])
+
+  const fileId = await fetchUploadFileWithToken('/api/upload', formData)
+  console.log(formDetail)
   console.log(fileId)
-  if(!fileId) {
+  if (!fileId) {
     console.log('image upload error')
     return
   }
+  console.log('upload success')
 }
 
 const confirmForm = () => {
   const { name } = formDetail
-  if (!name ) {
+  if (!name) {
     console.log('欄位不得為空')
     return
   }
-  emit('confirmForm', {...formDetail})
+  emit('confirmForm', { ...formDetail })
 }
 </script>
